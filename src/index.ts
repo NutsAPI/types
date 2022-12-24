6e55f12f82ada4c728fda667c3071b6537bf9238
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-12-25 08:08:14 +0900

    Feature/base64url (#18)
    * Create base64url.ts
    
    * Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { HTTP_REQUEST_METHODS, HttpRequestMethod } from './httpMethods';
 export { ApiSchemaBase, AnyZodLike, ApiRequestBase, ApiResponseBase } from './apiSchemaBase';
+export { toBase64, toBase64URL } from './base64url';
 export * from './converter';
